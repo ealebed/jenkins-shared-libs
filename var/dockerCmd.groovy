215c89aaea822730ebf989420dfd7bcc3e1c40e5
Yevhen Lebid <yevhen.lebid@loopme.com>
2018-07-13 17:27:00 +0300

    Update dockerCmd.groovy

--- var/dockerCmd.groovy
+++ var/dockerCmd.groovy
@@ -2,6 +2,8 @@
 
 def call(args) {
       assert args != null
+      sh '''echo ${args}'''
+      sh '''ls -al'''
 //      return sh(script: "sudo docker ${args}", returnStdout: true)
       sh '''sudo docker ${args}'''
   }
