981b0bb2dbc644cd1160600ae0b4b917cb0e52ff
Yevhen Lebid <yevhen.lebid@loopme.com>
2018-07-13 17:33:23 +0300

    Delete dockerCmd.groovy

--- var/dockerCmd.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy  
-
-def call(args) {
-      assert args != null
-      echo "Arguments are ${args}"
-//      return sh(script: "sudo docker ${args}", returnStdout: true)
-      return (sh '''sudo docker ${args}''', returnStdout: true)
-  }
