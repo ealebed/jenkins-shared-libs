4444336cb0aee43b1d495e30cfff54106718764d
Yevhen Lebid <yevhen.lebid@loopme.com>
2018-07-13 17:55:07 +0300

    Update createDockerfile.groovy

--- vars/createDockerfile.groovy
+++ vars/createDockerfile.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
 def call() {
-  sh(script: "def Dockerfile = libraryResource 'Dockerfile'")
-  sh(script: "writeFile file: 'Dockerfile', text: Dockerfile")
-  sh(script: "ls -la")
-  sh(script: "cat Dockerfile")
+  echo "Hello!"
+//  sh(script: "def Dockerfile = libraryResource 'Dockerfile'")
+//  sh(script: "writeFile file: 'Dockerfile', text: Dockerfile")
+//  sh(script: "ls -la")
+//  sh(script: "cat Dockerfile")
 }
