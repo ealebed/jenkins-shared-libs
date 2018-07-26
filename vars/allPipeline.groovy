7261c0a74a9623f2b0a23449dbcdb1fe398fe3b1
Yevhen Lebid <yevhen.lebid@loopme.com>
2018-07-26 10:39:57 +0300

    Update allPipeline.groovy

--- vars/allPipeline.groovy
+++ vars/allPipeline.groovy
@@ -1,41 +1,37 @@
 def call(body) {
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
 
-    pipeline {
-        agent any
+  pipeline {
+    agent any
 
-        stages {
-            stage('Checkout') {
-                steps {
-                    checkout scm
-                }
-            }
-            stage('Get Dockerfile') {
-                steps {
-                		script {
-			                  def tmpFile = libraryResource 'Dockerfile'
-			                  writeFile file: 'Dockerfile', text: tmpFile
-                  			    sh '''
-                    				ls -la
-                    				cat Dockerfile
-                  		  	    '''
-                		}
-                }
-            }
-            stage('Build') {
-                steps {
-                		script {
-					sh '''
-		                     docker version
-		                     docker build -t ealebed/hn:1.0.313 .
-		                     docker image ls
-				    '''
-                    }
-                }
-            }
+    stages {
+      stage('Checkout') {
+        steps {
+          checkout scm
         }
+      }
+      stage('Get Dockerfile') {
+        steps {
+          script {
+            def tmpFile = libraryResource 'Dockerfile'
+            writeFile file: 'Dockerfile', text: tmpFile
+          }
+        }
+      }
+      stage('Build') {
+        steps {
+          script {
+            sh '''
+              docker version
+              docker build -t ealebed/hellonode:latest .
+              docker image ls
+            '''
+          }
+        }
+      }
     }
+  }
 }
